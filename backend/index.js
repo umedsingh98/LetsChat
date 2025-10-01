d62fef6a98eab854e6af90c602cdcffe4c58c70a
umedsingh98 <umedsingh0898@gmail.com>
2025-10-02 01:01:28 +0530

    Server port changes in index.js backend

--- backend/index.js
+++ backend/index.js
@@ -47,5 +47,11 @@ app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 await connectDB();
+
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log("Serveris running on port: " + PORT));
+}
+
+//Export server for vercel deployment
+export default server;
